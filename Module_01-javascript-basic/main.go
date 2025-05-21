0d2fa3abade43548bcc8a3824f66350df0bad890
Tilak Singh <155138205+tilakdotcom@users.noreply.github.com>
2025-05-21 23:26:57 +0530

    var

--- Module_01-javascript-basic/main.go
+++ Module_01-javascript-basic/main.go
@@ -5,3 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("hello from go lang")
 }
+
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "Tilak"
+    var age int = 22
+    var isDeveloper bool = true
+
+    fmt.Println(name, age, isDeveloper)
+}
