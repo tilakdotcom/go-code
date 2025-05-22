32f63f9ce98311908838a70d7c0b0633c35d2ca5
tilak-dev <tsingh99991@gmail.com>
2025-05-22 11:44:57 +0530

    simple variables defining

---
+++ Module_01-javascript-basic/3.variables/variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	//print string
+	var name string = "Tilak"
+	fmt.Println(name)
+
+	//infer type
+	// if u are not using var  dont declare or comment
+	// var user = "tilak"
+
+	//print int
+	var age int = 20
+	fmt.Println(age)
+
+	//print bool 
+	var isAdult bool=  true
+	fmt.Println(isAdult)
+
+	// short Hand to define type 
+	fullName := "Tilak Singh"
+	fmt.Println(fullName)
+
+
+	// this methods applies to rest of 
+	price := 53.54
+	fmt.Println(price)
+
+
+}
