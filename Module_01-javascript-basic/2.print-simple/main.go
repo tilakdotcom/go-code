abd764fbee39cc0cdcc900f72e5c39d59b2d7f52
tilak-dev <tsingh99991@gmail.com>
2025-05-22 11:23:57 +0530

    printing  simple  values

---
+++ Module_01-javascript-basic/2.print-simple/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int
+	fmt.Println(1)
+	fmt.Println(1+43)
+	//string
+	fmt.Println("Tilak singh")
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+	//float
+	fmt.Println(4.43)
+	fmt.Println(4.43 / 2.421)
+}
