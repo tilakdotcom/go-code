30eb45a31137f9cf715abf5c54d69c70a101aa1b
tilak-dev <tsingh99991@gmail.com>
2025-05-24 14:34:34 +0530

    Implement channel operations and goroutine synchronization in channel.go

---
+++ Module_04-go-advance/1.channels/channel.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//channels are block
+
+func deadLock() {
+	messageChan := make(chan string)
+
+	messageChan <- "ping" //blocking
+
+	msg := <-messageChan
+
+	fmt.Println(msg)
+}
+
+// sending channels's data
+func printNum(nums chan int) {
+	for num := range nums {
+		fmt.Println("this is num ", num)
+		time.Sleep(time.Second)
+	}
+}
+
+// goroutine synchronizer
+func task(done chan bool) {
+	defer func() {
+		done <- true
+	}()
+
+	fmt.Println("processing...")
+}
+
+// receiving channels's data
+func sum(result chan int, nums ...int) {
+	total := 0
+	for _, num := range nums {
+		total = total + num
+	}
+	result <- total
+}
+
+func main() {
+	// deadLock()
+	//sending channels data
+	// nums := make(chan int)
+
+	// go printNum(nums)
+
+	// for range 2 {
+	// 	nums <- rand.Intn(100)
+	// }
+
+	//receiving channels data
+
+	// result1 := make(chan int)
+
+	// go sum(result1, 32, 45, 134, 45, 23, 23, 43)
+
+	// res := <-result1 //blocking
+
+	// fmt.Println(res)
+
+	//goroutine synchronizer
+	done := make(chan bool)
+
+	go task(done)
+
+	<-done// for blocking the program
+
+}
